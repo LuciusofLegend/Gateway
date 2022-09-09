e4eeeac1f9603f99404844662a81d0c92b407ce3
LuciusofLegend <luciusiber@gmail.com>
2022-09-09 06:30:57 -0400

    Backup

--- mods/mapgen/init.lua
+++ mods/mapgen/init.lua
@@ -1,2 +1,2 @@
-minetest.register_alias("mapgen_stone", "nodes:stone")
-minetest.register_alias("mapgen_water_source", "nodes:water_source")
+minetest.register_alias("mapgen_stone", "sg_nodes:stone")
+minetest.register_alias("mapgen_water_source", "sg_nodes:water_source")
