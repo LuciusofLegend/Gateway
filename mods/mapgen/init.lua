bb8951446c53359373efa3112b532caa5b939b78
Lucius <luciusiber@gmail.com>
2022-09-16 08:52:29 -0400

    Rename to Gateway

--- mods/mapgen/init.lua
+++ mods/mapgen/init.lua
@@ -1,2 +1,2 @@
-minetest.register_alias("mapgen_stone", "sg_nodes:stone")
-minetest.register_alias("mapgen_water_source", "sg_nodes:water_source")
+minetest.register_alias("mapgen_stone", "gt_nodes:stone")
+minetest.register_alias("mapgen_water_source", "gt_nodes:water_source")
