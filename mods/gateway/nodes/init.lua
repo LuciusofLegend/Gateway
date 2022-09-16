741bcebbff3e29cc7e7b64d46068c23802143987
Lucius <luciusiber@gmail.com>
2022-09-16 16:09:23 -0400

    Getting Started

---
+++ mods/gateway/nodes/init.lua
@@ -0,0 +1,11 @@
+minetest.register_node("nodes:stone", {
+    desciption = "Stone",
+    tiles = {"nodes_stone.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("nodes:water_source", {
+    desciption = "Water Source",
+    tiles = {"nodes_water.png"},
+    groups = {}
+})
--- mods/mapgen/init.lua
+++
@@ -1,2 +0,0 @@
-minetest.register_alias("mapgen_stone", "gt_nodes:stone")
-minetest.register_alias("mapgen_water_source", "gt_nodes:water_source")
